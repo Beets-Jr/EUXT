46a97ff2bed596412662eab604e8a11b513ec070
yuutaDragon <guilherme.rodrigues@estudante.ufscar.br>
2023-03-26 15:41:28 -0300

    fix errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,18 +6,20 @@ import uxLerisLogo from '../../public/uxLerisLogo.svg'
 export default function Header() {
   return (
     <header className="bg-gold flex justify-between w-full items-center py-3 px-40 md:px-20">
-      <div className="flex gap-5">
-        <Image
-          height={64}
-          width={64}
-          src={uxLerisLogo}
-          alt="Quadrado branco preenchido com UX na parte de cima e leris na parte de baixo, UX possui um contorno dourado"
-        />
-        <div className="flex flex-col justify-center text-gray-50">
-          <h1 className="font-bold">EUXT</h1>
-          <p className="">Exploring User eXperience Tools</p>
+      <Link href="/">
+        <div className="flex gap-5">
+          <Image
+            height={64}
+            width={64}
+            src={uxLerisLogo}
+            alt="Quadrado branco preenchido com UX na parte de cima e leris na parte de baixo, UX possui um contorno dourado"
+          />
+          <div className="flex flex-col justify-center text-gray-50">
+            <h1 className="font-bold">EUXT</h1>
+            <p className="">Exploring User eXperience Tools</p>
+          </div>
         </div>
-      </div>
+      </Link>
       <nav>
         <ul className="flex gap-5 text-gray-50 font-bold">
           <li>
